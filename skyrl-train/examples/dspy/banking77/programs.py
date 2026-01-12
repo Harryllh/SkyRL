8d6ed196eaf280bc155c90cfa9f088ece520e046
J-Ch-n <jiashuchen@berkeley.edu>
2026-01-12 17:48:15 +0000

    update: change dspy lm setting

--- skyrl-train/examples/dspy/banking77/programs.py
+++ skyrl-train/examples/dspy/banking77/programs.py
@@ -20,6 +20,16 @@ class Banking77_intent_classifier(Banking77):
         self.intent_classifier_traces = []
         self.intents = []
         
+        self.intent_classifier.set_lm(dspy.LM(
+            model="openai/Qwen/Qwen2.5-1.5B",
+            api_base="http://127.0.0.1:8000/v1",
+            api_key="fake-key",
+            temperature=1.0,
+            model_type="chat",
+            max_tokens=4096,
+            cache=False,
+        ))
+        
     async def forward(self, example) -> str:
         text = example.get("text")
         intent = await self.intent_classifier.acall(text=text)
